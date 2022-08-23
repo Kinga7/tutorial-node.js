93f4dd01b4444fc7f3b0bf84abbf26b689ef94d3
Austin King <awkscbw@gmail.com>
2022-08-23 07:34:20 -0700

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const http = require(`http`);
+const _ = require(`lodash`)
 
-const server = http.createServer((req, res)=>{
-if(req.url === `/`){
-    res.end(`Welcome to our home page`)
-}
-if (req.url === `/about`) {
-    res.end(`Here is our short history`)
-}
-res.end(`
-<h1>0oops!</h1>
-<p> We can't seem to find the page that you are looking for </p>
-<a href="/">back home</a>
-`)
-})
-
-server.listen(5000) 
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items);
+console.log(newItems);
